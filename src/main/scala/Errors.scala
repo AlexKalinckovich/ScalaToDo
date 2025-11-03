38ae602edab4b01acd30968961b4b4c336aafc2a
AlexKalinckovich <aleckseykalinckovich@gmail.com>
2025-11-03 11:36:16 +0300

    1) Add CRUD endpoints 2) PostgresSQL data management 3) Add simple Validation

---
+++ src/main/scala/Errors.scala
@@ -0,0 +1,16 @@
+import cats.data.NonEmptyList
+import java.util.UUID
+
+sealed trait AppError extends Throwable
+
+case class ValidationErrors(messages: NonEmptyList[String]) extends AppError {
+    override def getMessage: String = messages.toList.mkString(", ")
+}
+
+case class TodoNotFound(id: UUID) extends AppError {
+    override def getMessage: String = s"Todo with id $id not found"
+}
+
+case class InvalidUuid(raw: String) extends AppError {
+    override def getMessage: String = s"Invalid UUID: $raw"
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,85 @@
 import cats.effect.{IO, IOApp}
 import cats.syntax.semigroupk.*
 import com.comcast.ip4s.*
-import org.http4s.HttpRoutes
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+import org.http4s.circe.CirceEntityCodec.*
 import org.http4s.dsl.io.*
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits.* 
+import org.http4s.implicits.*
+import org.http4s.{HttpApp, HttpRoutes}
+
+import java.util.UUID
+import scala.util.Try
 
 object Main extends IOApp.Simple {
 
-    private val helloWorldRoutes = HttpRoutes.of[IO] {
-        case GET -> Root / "hello" =>
-            Ok("Hello, World!")
+    import JsonCodecs.*
+    import Validator.*
+
+    private def parseUuid(id: String): IO[UUID] = {
+        IO.fromTry(
+                Try(UUID.fromString(id)))
+            .adaptError {
+                case _: IllegalArgumentException => InvalidUuid(id)
+            }
     }
 
-    private val todoRoutes = HttpRoutes.of[IO] {
-        case GET -> Root / "todos" =>
-            Ok("Mock: Get ALL todos")
-        case GET -> Root / "todos" / id =>
-            Ok(s"Mock: Get todo by ID: $id")
-        case POST -> Root / "todos" =>
-            Ok("Mock: Create a new todo")
-        case PUT -> Root / "todos" / id =>
-            Ok(s"Mock: Update todo by ID: $id")
-        case DELETE -> Root / "todos" / id =>
-            Ok(s"Mock: Delete todo by ID: $id")
+    private def createRoutes(todoService: TodoService): HttpRoutes[IO] = {
+        val helloWorldRoutes = HttpRoutes.of[IO] {
+            case GET -> Root / "hello" =>
+                Ok("Hello, World!")
+        }
+
+        val todoRoutes = HttpRoutes.of[IO] {
+
+            case GET -> Root / "todos" =>
+                for {
+                    todos <- todoService.getAllTodos()
+                    resp <- Ok(todos.asJson)
+                } yield resp
+
+            case GET -> Root / "todos" / id =>
+                for {
+                    uuid <- parseUuid(id)
+                    todoOpt <- todoService.getTodoById(uuid)
+                    todo <- IO.fromOption(todoOpt)(TodoNotFound(uuid))
+                    resp <- Ok(todo.asJson)
+                } yield resp
+
+            case req @ POST -> Root / "todos" =>
+                for {
+                    createReq <- req.as[CreateTodoRequest]
+                    _ <- validate(createReq)
+                    todo <- todoService.createTodo(createReq)
+                    resp <- Created(todo.asJson)
+                } yield resp
+
+            case req @ PUT -> Root / "todos" / id =>
+                for {
+                    uuid <- parseUuid(id)
+                    updateReq <- req.as[UpdateTodoRequest]
+                    _ <- validate(updateReq)
+                    todoOpt <- todoService.updateTodo(uuid, updateReq)
+                    todo <- IO.fromOption(todoOpt)(TodoNotFound(uuid))
+                    resp <- Ok(todo.asJson)
+                } yield resp
+
+            case DELETE -> Root / "todos" / id =>
+                for {
+                    uuid <- parseUuid(id)
+                    deleted <- todoService.deleteTodo(uuid)
+                    resp <- if (deleted) NoContent() else IO.raiseError(TodoNotFound(uuid))
+                } yield resp
+        }
+
+        helloWorldRoutes <+> todoRoutes
     }
 
-    private val allRoutes = helloWorldRoutes <+> todoRoutes
+    private def notFoundApp: HttpApp[IO] = HttpRoutes.of[IO] {
+        case req =>
+            NotFound(ErrorResponse(s"Endpoint not found: ${req.method} ${req.uri.path}"))
+    }.orNotFound
 
     def run: IO[Unit] = {
         for {
@@ -35,14 +88,24 @@ object Main extends IOApp.Simple {
 
             _ <- Migrations.run(config.db)
 
-            _ <- EmberServerBuilder
-                .default[IO]
-                .withHost(ipv4"0.0.0.0")
-                .withPort(config.httpPort)
-                .withHttpApp(allRoutes.orNotFound)
-                .build
-                .use(_ => IO.never)
+            _ <- Database.transactor(config.db).use { xa =>
+                val todoRepository = TodoRepositoryPostgres(xa)
+                val todoService = new TodoService(todoRepository)
 
-        } yield () 
+                val routes = createRoutes(todoService)
+
+                val httpApp: HttpApp[IO] = routes.orNotFound
+
+                val handledApp = ErrorHandler(httpApp)
+
+                EmberServerBuilder
+                    .default[IO]
+                    .withHost(ipv4"0.0.0.0")
+                    .withPort(config.httpPort)
+                    .withHttpApp(handledApp)
+                    .build
+                    .use(_ => IO.never)
+            }
+        } yield ()
     }
 }
