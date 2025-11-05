7469d3be0916bc76a04ae3fd654337413344fb69
AlexKalinckovich <aleckseykalinckovich@gmail.com>
2025-11-05 11:42:53 +0300

    1) update implicit context to a given style

--- src/main/scala/codecs/JsonCodecs.scala
+++ src/main/scala/codecs/JsonCodecs.scala
@@ -10,31 +10,31 @@ import model.{CreateTodoRequest, Importance, UpdateTodoRequest, TodoResponse}
 
 object JsonCodecs {
 
-    implicit val uuidEncoder: Encoder[UUID] = Encoder.encodeString.contramap[UUID](_.toString)
-    implicit val uuidDecoder: Decoder[UUID] = Decoder.decodeString.map(UUID.fromString)
+    given uuidEncoder: Encoder[UUID] = Encoder.encodeString.contramap[UUID](_.toString)
+    given uuidDecoder: Decoder[UUID] = Decoder.decodeString.map(UUID.fromString)
 
-    implicit val instantEncoder: Encoder[Instant] = Encoder.encodeString.contramap[Instant](_.toString)
-    implicit val instantDecoder: Decoder[Instant] = Decoder.decodeString.map(Instant.parse)
+    given instantEncoder: Encoder[Instant] = Encoder.encodeString.contramap[Instant](_.toString)
+    given instantDecoder: Decoder[Instant] = Decoder.decodeString.map(Instant.parse)
 
-    implicit val localDateEncoder: Encoder[LocalDate] = Encoder.encodeString.contramap[LocalDate](_.toString)
+    given localDateEncoder: Encoder[LocalDate] = Encoder.encodeString.contramap[LocalDate](_.toString)
 
-    implicit val localDateDecoder: Decoder[LocalDate] = Decoder.decodeString.emap { s =>
+    given localDateDecoder: Decoder[LocalDate] = Decoder.decodeString.emap { s =>
         Try(LocalDate.parse(s)).toEither.left.map {
             case _: DateTimeParseException => s"Invalid date format for '$s'. Expected format: YYYY-MM-DD"
             case e => s"Invalid date: ${e.getMessage}"
         }
     }
 
-    implicit val importanceEncoder: Encoder[Importance] = Encoder.encodeString.contramap[Importance](_.toString)
+    given importanceEncoder: Encoder[Importance] = Encoder.encodeString.contramap[Importance](_.toString)
 
-    implicit val importanceDecoder: Decoder[Importance] = Decoder.decodeString.emap { s =>
+    given importanceDecoder: Decoder[Importance] = Decoder.decodeString.emap { s =>
         Try(Importance.valueOf(s)).toEither.left.map { _ =>
             val validValues = Importance.values.map(_.toString).mkString(", ")
             s"Invalid value '$s' for importance. Valid values are: $validValues"
         }
     }
 
-    implicit val createTodoRequestDecoder: Decoder[CreateTodoRequest] = deriveDecoder
-    implicit val updateTodoRequestDecoder: Decoder[UpdateTodoRequest] = deriveDecoder
-    implicit val todoResponseEncoder: Encoder[TodoResponse] = deriveEncoder
+    given createTodoRequestDecoder: Decoder[CreateTodoRequest] = deriveDecoder
+    given updateTodoRequestDecoder: Decoder[UpdateTodoRequest] = deriveDecoder
+    given todoResponseEncoder: Encoder[TodoResponse] = deriveEncoder
 }
